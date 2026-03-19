4925350679b1dbb5c4beefe4d4ab7820f47b21fc
VirajP1002 <Viraj.Patel@ons.gov.uk>
2026-03-19 12:09:02 +0000

    Add logline

--- surveys/surveys.go
+++ surveys/surveys.go
@@ -152,6 +152,7 @@ func GetAvailableSchemasFromCIR() []CIMetadata {
 
 	log.Printf("CIR API Base URL: %s", hostURL)
 	url := fmt.Sprintf("%s/v2/ci_metadata", hostURL)
+	log.Printf("I've accessed ci_metadata endpoint using: %s", url)
 
 	resp, err := client.Get(url)
 	if err != nil || resp.StatusCode != 200 {
