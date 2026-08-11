a670349959802ebf198025eadc257372a61d471e
R McCarthy <42928680+rmccar@users.noreply.github.com>
2026-08-11 12:24:29 +0100

    move to eslint 10 config

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        ignores: [
+            "node_modules/**",
+            "coverage/**",
+            "dist/**",
+            "megalinter-reports/**"
+        ]
+    },
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module"
+        },
+        rules: {
+            "no-unused-vars": "off",
+            "no-undef": "off"
+        }
+    }
+];
